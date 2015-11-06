737bee1c2985fbabf855c241003d0ef630220a0f
thomasb18 <bmtmail@comcast.net>
2015-11-05 17:39:13 -0800

    Random board generation.

--- app/src/main/java/group2/cs301/labyrinthgame/Labyrinth/Board.java
+++ app/src/main/java/group2/cs301/labyrinthgame/Labyrinth/Board.java
@@ -30,13 +30,94 @@ public class Board {
         int linesToAdd = 13;
         int teesToAdd = 6;
         int cornersToAdd = 15;
+        int treasureTick = 13;
+        int column = 1;
+        int row = 0;
+
+        for(int i = 0; i < 7; ++i) {
+            for(int j = 0; j < 7; ++j) {
+                gameTiles[i][j] = null;
+            }
+        }
+        extraTile = null;
 
         gameTiles[0][0] = new Tile(Tile.CORNER, Tile.RIGHT, true, false, false, false, 0);
-        gameTiles[2][0] = new Tile(Tile.TEE, Tile.DOWN, false, false, false ,false, 0);
-        gameTiles[4][0] = new Tile(Tile.TEE, Tile.DOWN, false, false,false,false, 0);
+        gameTiles[2][0] = new Tile(Tile.TEE, Tile.DOWN, false, false, false ,false, 1);
+        gameTiles[4][0] = new Tile(Tile.TEE, Tile.DOWN, false, false,false,false, 2);
         gameTiles[6][0] = new Tile(Tile.CORNER, Tile.DOWN, false, true, false ,false , 0);
-        gameTiles[0][2] = new Tile(Tile.TEE, Tile.RIGHT, false, false, false, false, 0);
-        gameTiles[2][2] = new Tile(Tile.TEE, Tile.RIGHT, false, false, false, false, 0);
+        gameTiles[0][2] = new Tile(Tile.TEE, Tile.RIGHT, false, false, false, false, 3);
+        gameTiles[2][2] = new Tile(Tile.TEE, Tile.RIGHT, false, false, false, false, 4);
+        gameTiles[4][2] = new Tile(Tile.TEE, Tile.DOWN, false, false, false, false, 5);
+        gameTiles[6][2] = new Tile(Tile.TEE, Tile.LEFT, false, false, false, false, 6);
+        gameTiles[0][4] = new Tile(Tile.TEE, Tile.RIGHT, false, false, false, false, 7);
+        gameTiles[2][4] = new Tile(Tile.TEE, Tile.UP, false, false, false, false, 8);
+        gameTiles[4][4] = new Tile(Tile.TEE, Tile.LEFT, false, false, false, false, 9);
+        gameTiles[6][4] = new Tile(Tile.TEE, Tile.LEFT, false, false, false, false, 10);
+        gameTiles[0][6] = new Tile(Tile.CORNER, Tile.UP, false, false, false, false, 0);
+        gameTiles[2][6] = new Tile(Tile.TEE, Tile.UP, false, false, false, false, 11);
+        gameTiles[4][6] = new Tile(Tile.TEE, Tile.UP, false, false, false, false, 12);
+        gameTiles[6][6] = new Tile(Tile.CORNER, Tile.LEFT, false, false, false, false, 0);
+
+        //add 34 randomly generated tiles
+        for(int i = 0; i < 35; ++i) {
+            int orientation = 0;
+            int type = 0;
+
+            boolean successful;
+
+            //get random tile type
+            do {
+                int rand = (int) (Math.random() * 3);
+                if(rand == 0 && linesToAdd > 0) {
+                    linesToAdd--;
+                    type = Tile.LINE;
+                    successful = true;
+                }
+                else if (rand == 1 && cornersToAdd > 0) {
+                    cornersToAdd--;
+                    type = Tile.CORNER;
+                    successful = true;
+                }
+                else if (rand == 2 && teesToAdd > 0){
+                    teesToAdd--;
+                    type = Tile.TEE;
+                    successful = true;
+                }
+                else {
+                    successful = false;
+                }
+            } while (!successful);
+
+            switch ((int) (Math.random()*4)) {
+                case 1 : orientation = Tile.RIGHT;
+                    break;
+                case 2 : orientation = Tile.LEFT;
+                    break;
+                case 3 : orientation = Tile.DOWN;
+                    break;
+                case 4 : orientation = Tile.UP;
+                    break;
+            }
+
+            //create the tile in its array spot
+            if(i < 34) {
+                gameTiles[column][row] = new Tile(type, orientation, false, false, false, false, treasureTick);
+            }
+            else {
+                extraTile = new Tile(type, orientation, false, false, false, false, treasureTick);
+            }
+
+            //move row and column forward
+            column +=2;
+            if(column > 7) {
+                column = 0;
+                row++;
+                if(row % 2 == 0) {
+                    column++;
+                }
+            }
+            treasureTick++;
+        }
 
     }
 
--- app/src/main/java/group2/cs301/labyrinthgame/Labyrinth/LabyrinthGameState.java
+++ app/src/main/java/group2/cs301/labyrinthgame/Labyrinth/LabyrinthGameState.java
@@ -8,19 +8,19 @@ import group2.cs301.labyrinthgame.Game.infoMsg.GameState;
 public class LabyrinthGameState extends GameState {
     private int currentPlayer;
     private Board gameBoard;
-    private String[] player1Targets;
-    private String[] player2Targets;
-    private String[] player3Targets;
-    private String[] player4Targets;
+    private int[] player1Targets;
+    private int[] player2Targets;
+    private int[] player3Targets;
+    private int[] player4Targets;
 
     //creates a new gameState
     public LabyrinthGameState () {
         currentPlayer = 0;
         gameBoard = new Board();
-        player1Targets = new String[4];
-        player2Targets = new String[4];
-        player3Targets = new String[4];
-        player4Targets = new String[4];
+        player1Targets = new int[4];
+        player2Targets = new int[4];
+        player3Targets = new int[4];
+        player4Targets = new int[4];
     }
 
     //creates a deep copy of a given game state
@@ -29,22 +29,22 @@ public class LabyrinthGameState extends GameState {
 
         gameBoard = new Board(toCopy.gameBoard);
 
-        player1Targets = new String[toCopy.player1Targets.length];
+        player1Targets = new int[toCopy.player1Targets.length];
         for(int i = 0; i < player1Targets.length; i++) {
             player1Targets[i] = toCopy.player1Targets[i];
         }
 
-        player2Targets = new String[toCopy.player2Targets.length];
+        player2Targets = new int[toCopy.player2Targets.length];
         for(int i = 0; i < player2Targets.length; i++) {
             player2Targets[i] = toCopy.player2Targets[i];
         }
 
-        player3Targets = new String[toCopy.player3Targets.length];
+        player3Targets = new int[toCopy.player3Targets.length];
         for(int i = 0; i < player3Targets.length; i++) {
             player3Targets[i] = toCopy.player3Targets[i];
         }
 
-        player4Targets = new String[toCopy.player4Targets.length];
+        player4Targets = new int[toCopy.player4Targets.length];
         for(int i = 0; i < player4Targets.length; i++) {
             player4Targets[i] = toCopy.player4Targets[i];
         }
