05dc3d08034bf4897307953c977522681df2290a
Ben Rumptz <rumptz15@up.edu>
2015-11-07 14:44:44 -0800

    InsertTile now is working as intended

--- app/src/main/java/group2/cs301/labyrinthgame/Labyrinth/Board.java
+++ app/src/main/java/group2/cs301/labyrinthgame/Labyrinth/Board.java
@@ -152,16 +152,16 @@ public class Board {
             //moves across the row from 0 to 6, moving tiles while it goes
             for(int x = 0; x < 7; x++){
                 tempTile = gameTiles[x][yy];
-                if(x == 0) gameTiles[x][yy] = extraTile;
-                else gameTiles[x][yy] = tempTile;
+                gameTiles[x][yy] = extraTile;
+                extraTile = tempTile;
             }
         }
         else if(xx == 6){
             //moves across the row from 6 to 0, moving tiles while it goes
             for(int x = 6; x > -1; x--){
                 tempTile = gameTiles[x][yy];
-                if(x == 6) gameTiles[x][yy] = extraTile;
-                else gameTiles[x][yy] = tempTile;
+                gameTiles[x][yy] = extraTile;
+                extraTile = tempTile;
             }
         }
         else if(yy == 0){
@@ -169,18 +169,17 @@ public class Board {
             for(int y = 0; y < 7; y++){
                 tempTile = gameTiles[xx][y];
                 if(y == 0) gameTiles[xx][y] = extraTile;
-                else gameTiles[xx][y] = tempTile;
+                extraTile = tempTile;
             }
         }
         else if(yy == 6){
             //moves up the column from 6 to 0, moving tiles while it goes
             for(int y = 6; y > -1; y--){
                 tempTile = gameTiles[xx][y];
-                if(y == 6) gameTiles[xx][y] = extraTile;
-                else gameTiles[xx][y] = tempTile;
+                gameTiles[xx][y] = extraTile;
+                extraTile = tempTile;
             }
         }
-        extraTile = tempTile;
     }//insertExtraTile
 
     /**
