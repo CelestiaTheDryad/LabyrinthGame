8ee915d7dd22b98ca9c44ca26a9fad96fdcca3dd
Ben Rumptz <rumptz15@up.edu>
2015-12-01 16:00:18 -0800

    Easy AI & MoveAction updates
    MoveAction - follows the same constructor setup as all other GameActions
    
    EasyAI - Inserts tiles now standardized
    Can move to tile containing current treasure
    gets lost if the treasure is not highlighted

--- app/src/main/java/group2/cs301/labyrinthgame/Labyrinth/EasyAIPlayer.java
+++ app/src/main/java/group2/cs301/labyrinthgame/Labyrinth/EasyAIPlayer.java
@@ -16,6 +16,7 @@ public class EasyAIPlayer extends GameComputerPlayer {
     private boolean isActionInsert;
     private final int[] legalXInsertSpots = {1, 3, 5};
     private final int[] legalYInsertSpots = {1, 3, 5};
+    private LabyrinthGameState myState;
 
 
     public EasyAIPlayer(){
@@ -34,16 +35,44 @@ public class EasyAIPlayer extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
+        // use the info we are given to init our gamestate
+        if(info instanceof LabyrinthGameState)
+            myState = (LabyrinthGameState) info;
+        else
+        // if we are given a non-gamestate GameInfo, return for now
+            return;
+
         //First Action to send is Insert
         if(isActionInsert){
             Random gen = new Random();
-            int xx = gen.nextInt(3);
-            int yy = gen.nextInt(3);
+            int[] val = Board.INSERT_LOCATIONS[gen.nextInt(12)];
+            int xx = val[0];
+            int yy = val[1];
             game.sendAction(new InsertTileAction(this, xx, yy));
             isActionInsert = false;
         }
         else{
             //if we are not inserting, we must be moving
+            int myPlayerNum = myState.getCurrentPlayer();
+            PlayerData myData = myState.getPlayers().get(myPlayerNum);
+            myState.highlightToMove(myPlayerNum);
+
+            //first get all my tiles
+            //loop through each one at a time
+            //if the tile is highlighted and has my treasure, go there and stop
+            //if it is highlighted and farther away than the last highlighted tile, go there  **FIND A WAY TO CALCULATE DISTANCE FROM CURR TILE
+            Board currBoard = myState.getGameBoard();
+            for(int x = 0; x < 7; x++){
+                for(int y = 0; y < 7; y++){
+                    Tile thisTile = currBoard.getTile(x,y);
+                    if(thisTile.isHighlighted() && (thisTile.getTreasure() == myData.getCurrentTreasure())){
+                        game.sendAction(new MoveAction(this, x, y));
+                        break;
+                    }
+                }
+            }
+
+
         }
     }
 }
